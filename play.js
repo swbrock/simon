41a006e2e3a7b4d1676523278de7d709b8fd01f1
swbrock <90523707+swbrock@users.noreply.github.com>
2023-03-06 13:01:23 -0700

    add sound

--- play.js
+++ play.js
@@ -1,5 +1,5 @@
 const btnDescriptions = [
-    { file: 'sound.mp3', hue: 120 },
+    { file: 'sound1.mp3', hue: 120 },
     { file: 'sound2.mp3', hue: 0 },
     { file: 'sound3.mp3', hue: 60 },
     { file: 'sound4.mp3', hue: 240 },
